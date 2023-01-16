8e1d3eae6e4655d84f160a209be659078dc7b79c
Ioana Livia <ioanaaliviaa@gmail.com>
2023-01-16 19:19:11 +0200

    Updated resolve functions for UserType

--- src/graphql/types/UserType.js
+++ src/graphql/types/UserType.js
@@ -1,5 +1,9 @@
 const {GraphQLObjectType, GraphQLString, GraphQLList} = require("graphql");
+const { Op } = require("sequelize");
 const {GraphQLInt} = require("graphql/type");
+
+const tweetType = require("./TweetType");
+const models = require("../../models");
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -8,21 +12,51 @@ const UserType = new GraphQLObjectType({
     username: {type: GraphQLString},
     name: {type: GraphQLString},
     tweets: {
-      type: new GraphQLList(require("./TweetType")),
+      type: new GraphQLList(tweetType),
       resolve(parent, args) {
-        return null; // TODO
+        return models.Tweet.findAll({ where:{author: parent.id}});
       }
     },
     following: {
-      type: new GraphQLList(UserType),
-      resolve(parent, args) {
-        return null; // TODO
+        type: new GraphQLList(UserType),
+        async resolve(parent, args) {
+
+          const followingIds = await models.UserFollowers.findAll({
+            where: {
+              followerId : parent.id
+            }
+          });
+
+          const filteredResults = await models.User.findAll({
+            where: {
+              id: {
+                [Op.in]: followingIds.map(item => item.userId)
+              }
+            }
+          });
+
+          return filteredResults;
       }
     },
     followers: {
-      type: new GraphQLList(UserType),
-      resolve(parent, args) {
-        return null; // TODO
+        type: new GraphQLList(UserType),
+        async resolve(parent, args) {
+
+          const followersIds = await models.UserFollowers.findAll({
+            where: {
+              userId : parent.id
+            }
+          });
+
+          const filteredResults = await models.User.findAll({
+            where: {
+              id: {
+                [Op.in]: followersIds.map(item => item.followerId)
+              }
+            }
+          });
+
+          return filteredResults;
       }
     },
   })
