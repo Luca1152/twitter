407cb58fca4ce06954e5ad0972adb105213bf556
Luca Trușcă <t.luca1152@gmail.com>
2023-01-17 18:49:23 +0200

    Add GraphQL playground middleware

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const JWTMiddleware = require('./middlewares/JWTMiddleware');
 const {Schema} = require('./graphql');
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default
 
 const app = express();
 
 app.use(express.json());
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 app.use('/graphql', JWTMiddleware, graphqlHTTP({
   schema: Schema,
-  graphiql: true,
 }));
 
 app.listen(8080, () => {
